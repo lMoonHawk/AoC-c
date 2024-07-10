4d92c6090f78d4a98cd6bdeb9bbe7d16a882cf4a
lMoonHawk <lmoonhawk@gmail.com>
2024-07-10 12:42:31 +0200

    add ability to call point component by index in geom.h

--- common/geom.h
+++ common/geom.h
@@ -3,8 +3,14 @@
 
 #include <stdbool.h>
 
-typedef struct { int x, y; } Point2;
-typedef struct { int x, y, z; } Point3;
+typedef union {
+    struct { int x, y; };
+    int components[2];
+} Point2;
+typedef union {
+    struct { int x, y, z; };
+    int components[3];
+} Point3;
 typedef struct { Point2 min, max; } P2Bounds;
 typedef struct { Point2 start, end; } Seg2;
 
@@ -60,33 +66,8 @@ int p2dist(Point2 p1, Point2 p2) {
 }
 
 int p3dist(Point3 p1, Point3 p2) {
-    return abs(p1.x - p2.x) + abs(p1.y - p2.y) + + abs(p1.z - p2.z);
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y) + +abs(p1.z - p2.z);
 }
 
-bool get_line_intersection(float p0_x, float p0_y, float p1_x, float p1_y,
-    float p2_x, float p2_y, float p3_x, float p3_y, float* i_x, float* i_y)
-{
-    float s1_x, s1_y, s2_x, s2_y;
-    s1_x = p1_x - p0_x;
-    s1_y = p1_y - p0_y;
-    s2_x = p3_x - p2_x;
-    s2_y = p3_y - p2_y;
-
-    float s, t;
-    s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
-    t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
-
-    if (s >= 0 && s <= 1 && t >= 0 && t <= 1)
-    {
-        // Collision detected
-        if (i_x != NULL)
-            *i_x = p0_x + (t * s1_x);
-        if (i_y != NULL)
-            *i_y = p0_y + (t * s1_y);
-        return 1;
-    }
-
-    return 0; // No collision
-}
 
 #endif // GEOM_IMPLEMENTATION
